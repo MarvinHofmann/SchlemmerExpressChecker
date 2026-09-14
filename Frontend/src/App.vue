303cff070203599d9dc2fec9e78c9250cd18d508
MarvinHofmann <82021987+MarvinHofmann@users.noreply.github.com>
2026-09-14 11:25:56 +0200

    Update App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -3,23 +3,13 @@
   <div class="container mt-5 pt-5 ct d-flex justify-content-center">
     <div class="row">
       <h2 class="text-center text-white mb-4 mt-5">Nächste Überprüfung: {{ this.nextCheck }}</h2>
-      <div class="col-lg-6">
+      <div class="col-lg-12">
         <div class="card text-white bg-dark mb-3" style="min-width: 22rem;">
-          <div class="card-header">Mord im Schlemmer Express</div>
+          <div class="card-header">LUMAGICA Meran</div>
           <div class="card-body">
             <h5 v-if="!this.mordChanged" class="card-title text-success">Keine Veränderungen</h5>
             <h5 v-else class="card-title text-danger">Veränderung Erkannt</h5>
-            <a href="https://www.schwaebische-waldbahn.de/sonderfahrten/mord-im-schlemmerexpress">Webseite</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-6">
-        <div class="card text-white bg-dark mb-3" style="min-width: 22rem;">
-          <div class="card-header">Normaler Schlemmerexpress</div>
-          <div class="card-body">
-            <h5 v-if="!this.normChanged" class="card-title text-success">Keine Veränderungen</h5>
-            <h5 v-else class="card-title text-danger">Veränderung Erkannt</h5>
-            <a href="https://www.schwaebische-waldbahn.de/sonderfahrten/der-schlemmerexpress">Webseite</a>
+            <a href="https://lumagica.com/de/standorte/meran#intro">Webseite</a>
           </div>
         </div>
       </div>
@@ -39,10 +29,9 @@ export default {
   },
   methods: {
     async fetchValues() {
-      let res = await axios.get("https://schlemmer.zimmermatic.de/checkPages").then((response) => response.data)
+      let res = await axios.get("192.168.0.138:4545").then((response) => response.data)
       console.log(res);
       this.mordChanged = res.res0.change;
-      this.normChanged = res.res1.change;
     },
     setupAll() {
       setInterval(() => {
